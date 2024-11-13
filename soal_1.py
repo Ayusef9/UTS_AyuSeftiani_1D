a113083b95f104516b6dbfcb9aeb065449f56230
Ayusef9 <ayu32194@gmail.com>
2024-11-13 12:02:27 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,8 @@
+nilai_a = int(input("Masukan Nilai A :"))
+nilai_b = int(input("Masukan Nilai B : "))
+penjumlahan = nilai_a + nilai_b
+pengurangan = nilai_a - nilai_b
+perkalian = nilai_a * nilai_b
+print(f"Hasil Nilai Jumlah : {penjumlahan}")
+print(f"Hasil Nilai Kurang : {pengurangan}")
+print(f"Hasil Nilai Bagi : {perkalian}")
